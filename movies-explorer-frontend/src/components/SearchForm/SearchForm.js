0e0069bf45c8a899d1de36110d2e2028a21df614
Xenoxil <xilus1127@gmail.com>
2022-06-24 06:15:37 +0300

    подключить шрифты

--- movies-explorer-frontend/src/components/SearchForm/SearchForm.js
+++ movies-explorer-frontend/src/components/SearchForm/SearchForm.js
@@ -7,9 +7,9 @@ import movieTypeSwitcher from '../../images/smalltumb.png'
 function SearchForm(props) {
     return (
         <section className='searchSection'>
-          <form className='searchForm'>
+          <form className='searchForm' >
             <img src={loupePic} alt='Значок лупы' className='searchForm__loupePic'/>
-            <input className='searchForm__input' placeholder='Фильм'/>
+            <input className='searchForm__input' placeholder='Фильм' required/>
             <button className='searchForm__searchBtn'><img src={searchBtn} alt='Кнопка поиска'/></button>
             <button className='searchForm__movieTypeBtn'><img src={movieTypeSwitcher} alt='Кнопка переключения типа фильма'/></button>
             <p className='searchForm__movieType'>{props.isSwitched ?  `Полнометражки` :`Короткометражки`}</p>
